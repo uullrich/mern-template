65fd93f07cc62ef8eda68ac20b7a78990de8ac38
Uwe Ullrich <uwe.ullrich@pagnos.com>
2024-01-24 15:36:57 +0100

    feat: added assertions for create user scenario

--- backend/test/integration/features/User.feature
+++ backend/test/integration/features/User.feature
@@ -3,5 +3,6 @@ Feature: User endpoint operations
   Scenario: Add a new user to the database
     Given the request body with the valid email address test@test.com
     And the request body contains a valid first name: Max
+    And the request body contains a valid last name: Mustermann
     When the request is sent to the user creation endpoint
     Then a new user is created in the database
--- backend/test/integration/step_definitions/User.steps.ts
+++ backend/test/integration/step_definitions/User.steps.ts
@@ -2,6 +2,7 @@ import { Given, Then, When } from "@cucumber/cucumber";
 import DemoWorld from "../DemoWorld";
 import HttpRequester from "../util/HttpRequester";
 import { RequestConfigBuilder } from "../util/builder/RequestConfigBuilder";
+import { expect } from "expect";
 
 Given("the request body with the valid email address {word}", function (this: DemoWorld, email: string) {
   this.userBuilder.withEmail(email);
@@ -18,9 +19,13 @@ Given("the request body contains a valid last name: {word}", function (this: Dem
 When("the request is sent to the user creation endpoint", async function (this: DemoWorld) {
   const user = this.userBuilder.build();
   const request = new RequestConfigBuilder().withMethod("POST").withUrl("/api/user").withRequestBody(user).build();
-  this.response = await HttpRequester.sendRequest(request);
+  this.response = await HttpRequester.sendRequest<{ id: string }>(request);
 });
 
 Then("a new user is created in the database", function (this: DemoWorld) {
-  //console.log(this.response);
+  expect(this.response.status).toBe(201);
+  expect(this.response.data).toBeDefined();
+
+  const responsePayload = this.response.data as { id: string };
+  expect(responsePayload.id).toBeDefined();
 });
